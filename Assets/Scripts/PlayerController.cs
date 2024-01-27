7ed23649e1fd3e231f77e979c6378322e9ee3651
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-27 21:14:49 +0000

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3[] points;
+
+    private bool canMove;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
