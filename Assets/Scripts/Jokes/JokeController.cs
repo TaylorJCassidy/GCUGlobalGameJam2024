daed314a06214077a29597e9c3ecb03c21102139
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-27 19:44:19 +0000

    disable jokes spawning on scene start

--- Assets/Scripts/Jokes/JokeController.cs
+++ Assets/Scripts/Jokes/JokeController.cs
@@ -24,8 +24,6 @@ public class JokeController : MonoBehaviour
     {
         jokeController = this;
         jokes = getAllJokes();
-        spawnJokes();
-        spawnFullJoke();
     }
 
     public bool addSelectedJoke(JokePiece jokePiece)
