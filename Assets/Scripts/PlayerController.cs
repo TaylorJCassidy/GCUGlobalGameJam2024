d9913d75a3c2336ce054eae5034c64a4671f53b7
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-27 21:53:05 +0000

    add player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector3[] points;
+    [Min(1f)]
+    public float speed;
+    public Vector3[] positions;
+    private int currentPosition = 1; //middle
 
-    private bool canMove;
+    private bool canMove = true;
+
+    public bool CanMove { get => canMove; set => canMove = value; }
 
     // Update is called once per frame
     void Update()
     {
-        
+        processUserInput();
+    }
+
+    void processUserInput() {
+        if (canMove) {
+            if (Input.GetKeyDown(KeyCode.A)) {
+                if (currentPosition - 1 >= 0) --currentPosition;
+            }
+            else if (Input.GetKeyDown(KeyCode.D)) {
+                if (currentPosition + 1 < positions.Length) ++currentPosition;
+            }
+        }
+        move(positions[currentPosition]);
+    }
+
+    void move(Vector3 position) {
+        Vector3 lerpPos = Vector3.Lerp(transform.position, position, Time.deltaTime * speed);
+        transform.position = lerpPos;
     }
 }
