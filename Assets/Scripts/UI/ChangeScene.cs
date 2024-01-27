dc6888e4e192e9266ca33125ebbd414fe78a6051
FelizJueves <jamiedorrens@gmail.com>
2024-01-27 23:57:56 +0000

    forgot a bit
    I'm dumb

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -10,4 +10,8 @@ public class ChangeScene : MonoBehaviour
     {
         SceneManager.LoadScene("Main Scene");
     }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
