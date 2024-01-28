b146bcd60b2cda8fea20570cda598922444bf3d6
TaylorJCassidy <taylorjcassidy1@gmail.com>
2024-01-28 12:39:44 +0000

    make rotation and positions public, add new teleport method

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,27 +4,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Vector3[] positions;
+    public Vector3[] rotations;
+    private ObjectTeleporter objectTeleporter;
 
-    ObjectTeleporter objectTeleporter;
     void Awake() {
-        Vector3[] positions = {
-            new Vector3(0f, 3.2f, -5.53f),
-            new Vector3(5f, 7f, 0f),
-            new Vector3(0f, 5.5f, 8f)
-        };
-        Vector3[] rotations = {
-            new Vector3(0f, 0f ,0f),
-            new Vector3(30f, 315f, 0f),
-            new Vector3(20f, 180f, 0f)
-        };
         objectTeleporter = new ObjectTeleporter(positions, rotations, this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetButtonDown("Fire1")) { //TODO change button to change camera
-        //    objectTeleporter.Teleport(true);
-        //}
+        // if (Input.GetButtonDown("Fire1")) { //TODO change button to change camera
+        //    objectTeleporter.Teleport(1);
+        // }
     }
 }
