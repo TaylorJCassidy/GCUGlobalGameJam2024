e80d0492be479e013718c99bc9aec8aff505b723
Ben Reid <70808579+Cromby5@users.noreply.github.com>
2024-01-28 01:02:01 +0000

    Squashed commit of the following:
    commit a8db456c6165b7a0ce76c27d08257567005b3ece
    Merge: 354c7cb e24cc78
    Author: Taylor Cassidy <58339439+TaylorJCassidy@users.noreply.github.com>
    Date:   Sun Jan 28 00:58:10 2024 +0000
    
        Merge pull request #8 from TaylorJCassidy/punchlines
    
        Punchlines
    
    commit e24cc7884fabc2477bde9886fdac001546704755
    Author: TaylorJCassidy <taylorjcassidy1@gmail.com>
    Date:   Sun Jan 28 00:54:17 2024 +0000
    
        move timer to param
    
    commit 98d834080f5b86d58401291e3bd8a9ac512967c9
    Author: TaylorJCassidy <taylorjcassidy1@gmail.com>
    Date:   Sun Jan 28 00:44:06 2024 +0000
    
        seperate joke and punchline
    
    commit 354c7cbdc0c20e6c6300823c862dba18c034106f
    Merge: 92ef8ee dc6888e
    Author: Taylor Cassidy <58339439+TaylorJCassidy@users.noreply.github.com>
    Date:   Sun Jan 28 00:09:21 2024 +0000
    
        Merge pull request #7 from TaylorJCassidy/new-ui
    
        New UI
    
    commit dc6888e4e192e9266ca33125ebbd414fe78a6051
    Author: FelizJueves <jamiedorrens@gmail.com>
    Date:   Sat Jan 27 23:57:56 2024 +0000
    
        forgot a bit
    
        I'm dumb
    
    commit 2e15bb87b6a333238358f77b0189be5ab19a5cef
    Author: FelizJueves <jamiedorrens@gmail.com>
    Date:   Sat Jan 27 23:56:09 2024 +0000
    
        MainMenu
    
        Main Menu scene that will take the player to the game when the build settings are sorted

--- Assets/Scripts/Jokes/Joke.cs
+++ Assets/Scripts/Jokes/Joke.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewJoke", menuName = "Jokes/New Joke", order = 1)]
 public class Joke : ScriptableObject
 {
-    public string fullJoke;
+    public string joke;
+    public string punchline;
     public string[] jokeKeywords;
 }
